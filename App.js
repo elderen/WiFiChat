fe3353024d38dbd2113423cb7df1cd702e94b6a7
Elderen Lee <elderen2@gmail.com>
2019-06-07 17:44:01 -0400

    Successfully transferred working WiFiChat app into clean bare minimum working slate

--- App.js
+++ App.js
@@ -1,12 +1,55 @@
+window.navigator.userAgent = 'react-native';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
+import Prompt from 'rn-prompt';
+import LobbyChat from './lobbyChat.js'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      name: 'Anon',
+      promptVisible: true
+    };
+  }
+  componentDidMount = () => {
+    this.setState({
+      promptVisible: true
+    })
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <LobbyChat name={this.state.name} />
+
+        <Prompt
+          title="What is Your Username"
+          onSubmit={(data) => {
+            if (data !== '') {
+              this.setState({
+                name: data,
+                promptVisible: false
+              })
+            } else {
+              this.setState({
+                promptVisible: false
+              })
+            }
+          }}
+          onCancel={() => {
+            this.setState({
+              name: 'Anon',
+              promptVisible: false
+            })
+          }}
+          visible={this.state.promptVisible}
+          placeholder="Anonymous"
+        />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
@@ -15,5 +58,10 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    fontWeight: 'bold',
   },
+  words: {
+    fontSize: 30,
+    color: 'orange',
+  }
 });
